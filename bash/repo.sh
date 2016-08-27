2d0f9017a10d75663a396b432387a18c82bfe0d9
Nathan Bullock <nathanbullock@gmail.com>
2016-08-27 09:00:00 -0400

    Make repo commands more tolerant and add aliases

--- bash/repo.sh
+++ bash/repo.sh
@@ -1,23 +1,45 @@
 # Repository commands.
 
+# List all the git repos in $DOTBASH/..
+__repo_ls () {
+  for dir in ${DOTBASH}/../*; do
+    [ -d "$dir"/.git ] || continue
+    echo "$dir"
+  done
+}
+
+# List all the git repos in $DOTBASH/.. that are tracking a remote repository.
+__repo_ls_remote () {
+  for dir in $(__repo_ls); do
+    git -C "$dir" config --get remote.origin.url >/dev/null || continue
+    echo "$dir"
+  done
+}
+
 __repo_status () {
-  ls | xargs -P 10 -I{} git -C {} fetch
+  __repo_ls_remote | xargs -P 10 -I{} git -C {} fetch
 
   local COLOR='\033[0;34m'
   local NO_COLOR='\033[0m'
 
-  for dir in *; do
-    if [ -d "$dir" ]; then
-      echo -e "${COLOR}${dir}...\033[0m"
-      cd "$dir"
-      git status -sb
-      echo
-      git log origin/master..
-      echo
-      cd ..
-    fi
+  for dir in $(__repo_ls); do
+    [ -d "$dir" ] || continue
+
+    echo -e "${COLOR}${dir}...\033[0m"
+    git -C "$dir" status -sb
+    echo
+    git -C "$dir" config --get remote.origin.url >/dev/null && \
+        git -C "$dir" log origin/master..
+    echo
   done
 }
-alias rpull='( cd $DOTBASH/..; ls | xargs -P 10 -I{} git -C {} pull; );'
-alias rpush='( cd $DOTBASH/..; ls | xargs -I{} git -C {} push; );'
-alias rst='( cd $DOTBASH/..; __repo_status; );'
+
+alias rpull='__repo_ls_remote | xargs -P 10 -I{} git -C {} pull;'
+alias rpush='__repo_ls_remote | xargs -I{} git -C {} push;'
+alias rst='__repo_status;'
+alias rls='ls $DOTBASH/..'
+
+# Set up aliases for all repositories.
+for dir in $(__repo_ls); do
+  eval "alias $(basename $dir)='cd $(cd $dir; pwd)'"
+done
